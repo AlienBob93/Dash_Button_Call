1a4093079dc6311a499a8fcd539a3fd893ae4014
Prashant Rao <rao.prashant542@gmail.com>
2016-12-21 00:25:04 -0600

    added info to make probe work with the newer revesions of the buttons

--- probe_Button.py
+++ probe_Button.py
@@ -2,7 +2,7 @@ from scapy.all import *
 
 def arp_display(pkt):
   if pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe // disable if you have the DASH buttons with model JK29LP
       print "ARP Probe from: " + pkt[ARP].hwsrc
 
 print sniff(prn=arp_display, filter="arp", store=0, count=10)
